44a0ea603192d365b48c00c17a93fce803e1f312
Florencia - Ubuntu 20.04 <ing.florencia.bergesio@gmail.com>
2021-08-30 11:17:12 -0300

    Added routes management for get, post, delete for notes

--- server.js
+++ server.js
@@ -1,7 +1,24 @@
 const express = require('express');
 const app = express();
+const router = express.Router();
 
-app.use('/', (req, res) => res.send('Initial setup.'));
+app.use(router);
+
+router.get('/', ( req, res ) => {
+    res.send('Notekeeper');
+});
+
+router.get('/notes', ( req, res ) => {
+    res.send('Notes list');
+});
+
+router.post('/notes', ( req, res ) => {
+    res.send('Note added');
+});
+
+router.delete('/notes', ( req, res ) => {
+    res.send('Note deleted');
+});
 
 app.listen(3000);
 console.log('App is listening in http://localhost:3000');
