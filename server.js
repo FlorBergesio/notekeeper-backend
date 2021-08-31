d6595994ea69d53e9e140b6be06d58164c525115
Florencia - Ubuntu 20.04 <ing.florencia.bergesio@gmail.com>
2021-08-31 11:23:08 -0300

    CORS access control

--- server.js
+++ server.js
@@ -8,6 +8,15 @@ db.connect();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// CORS
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
 router(app);
 
 app.listen(3000);
